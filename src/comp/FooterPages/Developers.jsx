12d37770eeb461ff067eb4b3bf0ae57f6488ec78
ridhi-2801 <ridhi2801@gmail.com>
2021-12-03 17:57:00 +0530

    developers image

--- src/comp/FooterPages/Developers.jsx
+++ src/comp/FooterPages/Developers.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import dev from "../../images/dev.jpg"
 import { AiFillLinkedin } from "react-icons/ai";
 import {AiFillGithub} from "react-icons/ai";
+import aamir from "../../images/aamir.jpg";
+import ridhi from "../../images/ridhi.jpg";
+
 
 function Developers() {
     return (
@@ -12,7 +15,7 @@ function Developers() {
               <center>
                     <div className="d-box">
                     <img styles = {{paddingTop : 20}} className=" logo responsive-image mompreneurPadding-top"
-                                src="https://firebasestorage.googleapis.com/v0/b/tellmeastorymom-c17e3.appspot.com/o/aamir.jpg?alt=media&token=f517115f-d8bd-4896-ab96-9d4f528748b4"
+                                src={aamir}
                                  height="250" width="250" />
                         <h2 className="name-padding">Mohd Aamir</h2>
                             <h6>Full Stack Web Developer</h6>
@@ -24,7 +27,7 @@ function Developers() {
                     </div>
                     <div className="d-box">
                     <img styles = {{paddingTop : 20}} className=" logo responsive-image mompreneurPadding-top"
-                                src="https://firebasestorage.googleapis.com/v0/b/tellmeastorymom-c17e3.appspot.com/o/ridhi.jpg?alt=media&token=4e7f54cc-684e-435e-8670-844b5131c3a1"
+                                src={ridhi}
                                  height="250" width="250" />
                         <h2 className="name-padding">Ridhi Jain</h2>
                             <h6>Full Stack Web Developer</h6>
