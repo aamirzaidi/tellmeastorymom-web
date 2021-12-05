b28964eaeadfc0d86f583af52a58b7cdabb6b500
kaabil <aamir.zaidi5@gmail.com>
2021-12-05 22:37:52 +0530

    header

--- src/comp/Header/Header.jsx
+++ src/comp/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React,{useState} from "react";
 import { Link, useHistory} from 'react-router-dom';
 
 function Header() {
@@ -20,68 +20,65 @@ function Header() {
   }
 
   return (
-    
+
     <nav className="navbar navbar-expand-sm navbar-light top container-fluid">
-    <button className="navbar-toggler" type="" data-toggle="collapse" 
-    data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" 
-    aria-expanded="false" aria-label="Toggle navigation" >
-      <span className="navbar-toggler-icon"></span>
-      
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      
-      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <button className="navbar-toggler" type="" data-toggle="collapse"
+        data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-        <li className="nav-item active">
-          <Link  className ="nav-link top-color n menu-toggle" to="/"><h6 className="n">Home</h6> 
-          </Link><span className="sr-only">(current)</span>
-        </li>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <a className="nav-link top-color n" href ="/"><h6 className="n">Home <span className="sr-only">(current)</span></h6>
+            </a>
+          </li>
 
-        <li className="nav-item">
-          <nav >
-          <Link  className ="nav-link top-color n" to="/Mompreneur"><h6 className="n">Mompreneur</h6> 
-          </Link><span className="sr-only">(current)</span>
-          </nav>
-        </li>
+          <li className="nav-item">
+            <nav >
+              <a className="nav-link top-color n" href ="/Mompreneur" ><h6 className="n">Mompreneur</h6>
+              </a>
+            </nav>
+          </li>
 
-        <li className="nav-item" >
-          <nav>
-          <Link  className ="nav-link top-color n" to="/addStory"><h6 className="n">Submit a story</h6>  
-          </Link><span className="sr-only">(current)</span>
-          </nav>
-        </li>
+          <li className="nav-item" >
+            <nav>
+              <a className="nav-link top-color n" href ="/addStory"><h6 className="n">Submit a story</h6>
+              </a>
+            </nav>
+          </li>
 
-        <li className="nav-item">
-          <nav >
-          <Link  className ="nav-link top-color n" to="/guestposts"><h6 className="n">Guest Posts</h6> 
-          </Link><span className="sr-only">(current)</span>
-          </nav>
-        </li>
+          <li className="nav-item">
+            <nav >
+              <a className="nav-link top-color n" href ="/guestposts" ><h6 className="n">Guest Posts</h6>
+              </a>
+            </nav>
+          </li>
 
-        <li className="nav-item" >
-          <nav>
-          <Link  className ="nav-link top-color n" to="/exploreCategories"><h6 className="n">Categories</h6>  
-          </Link><span className="sr-only">(current)</span>
-          </nav>
-        </li>
+          <li className="nav-item" >
+            <nav>
+              <a className="nav-link top-color n" href ="/exploreCategories" ><h6 className="n">Categories</h6>
+              </a>
+            </nav>
+          </li>
 
-        <li className="nav-item" >
-          <nav>
-          <Link  className ="nav-link top-color n" to="/about"><h6 className="n">About</h6>  
-          </Link><span className="sr-only">(current)</span>
-          </nav>
-        </li>
+          <li className="nav-item" >
+            <nav>
+              <a className="nav-link top-color n" href ="/about"><h6 className="n">About</h6>
+              </a>
+            </nav>
+          </li>
 
-      </ul>
-      <form className="form-inline my-2 my-lg-0">
-        <input onChange={getSearchTerm}  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-        <button onClick={getSearchResults} 
-          className="btn btn-primary my-2 my-sm-0" 
-          type="submit">Search</button>
-      </form>
-    </div>
-
-  </nav>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input onChange={getSearchTerm}  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button onClick={getSearchResults}
+            className="btn btn-primary my-2 my-sm-0"
+            type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
   );
 }
 
